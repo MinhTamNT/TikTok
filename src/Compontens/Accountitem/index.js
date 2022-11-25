0d37762d834568ba8c24320c2222964a32bee39e
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2022-11-25 22:21:44 +0700

    Update Propper

---
+++ src/Compontens/Accountitem/index.js
@@ -0,0 +1,25 @@
+import styles from './Accountitem.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+function Accountitem() {
+    return (
+        <div className={cx('Wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tiktok-obj/1665757201241090~c5_300x300.webp?x-expires=1669557600&x-signature=0IjE%2Fy68dGn92475jvwMP1nEuX0%3D"
+                alt=""
+            />
+            <div className={cx('infor')}>
+                <h4 className={cx('name')}>
+                    <span>Minh Tam</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Marshall1502</span>
+            </div>
+        </div>
+    );
+}
+
+export default Accountitem;
--- src/Compontens/Layout/components/Header/index.js
+++ src/Compontens/Layout/components/Header/index.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+
+import { Wrapper as PropperWrapper } from '~/Compontens/Propper';
 import styles from './Header.module.scss';
 import images from '~/assets/image';
+import Accountitem from '~//Compontens/Accountitem/index';
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setInterval(() => {
+            setSearchResult([]);
+        }, 0);
+    }, []);
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -14,16 +27,34 @@ function Header() {
                     <img src={images.logo} alt="TikTok" />
                 </div>
                 {/* //Sreach */}
-                <div className={cx('search')}>
-                    <input placeholder="Search account and videos" spellCheck={false} />
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
+                <Tippy
+                    interactive={true}
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PropperWrapper>
+                                <h4 className={cx('search-title')}>Account</h4>
+                                <Accountitem />
+                                <Accountitem />
+                                <Accountitem />
+                                <Accountitem />
+                                <Accountitem />
+                            </PropperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search account and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
                 <div className={cx('action')}></div>
             </div>
         </header>
